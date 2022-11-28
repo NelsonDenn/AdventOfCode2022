c326e8a4138fe919fc841e8650ea75ff6bad8d39
Nelson Denn <15810147+NelsonDenn@users.noreply.github.com>
2022-11-28 16:23:17 -0500

    initial commit

---
+++ AdventOfCode2022/Day1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2022
+{
+	public class Day1
+	{
+		public static int Run()
+		{
+			bool isTest = true;
+			var data = isTest ? GetTestData() : GetData();
+
+			var result = RunPart1(data);
+			//var result = RunPart2(data);
+
+			return result;
+		}
+
+		private static int RunPart2(List<string> data)
+		{
+			return 0;
+		}
+
+		private static int RunPart1(List<string> data)
+		{
+			return 0;
+		}
+
+		private static List<string> GetTestData()
+		{
+			string data =
+@"2199943210
+3987894921
+9856789892
+8767896789
+9899965678";
+
+			return ParseData(data);
+		}
+
+		private static List<string> GetData()
+		{
+			//var data = new List<int>
+			//{
+			//	1,4,3,3,1,3,1,1,1,2,1,1,1,4,4,1,5,5,3,1,3,5,2,1,5,2,4,1,4,5,4,1,5,1,5,5,1,1,1,4,1,5,1,1,1,1,1,4,1,2,5,1,4,1,2,1,1,5,1,1,1,1,4,1,5,1,1,2,1,4,5,1,2,1,2,2,1,1,1,1,1,5,5,3,1,1,1,1,1,4,2,4,1,2,1,4,2,3,1,4,5,3,3,2,1,1,5,4,1,1,1,2,1,1,5,4,5,1,3,1,1,1,1,1,1,2,1,3,1,2,1,1,1,1,1,1,1,2,1,1,1,1,2,1,1,1,1,1,1,4,5,1,3,1,4,4,2,3,4,1,1,1,5,1,1,1,4,1,5,4,3,1,5,1,1,1,1,1,5,4,1,1,1,4,3,1,3,3,1,3,2,1,1,3,1,1,4,5,1,1,1,1,1,3,1,4,1,3,1,5,4,5,1,1,5,1,1,4,1,1,1,3,1,1,4,2,3,1,1,1,1,2,4,1,1,1,1,1,2,3,1,5,5,1,4,1,1,1,1,3,3,1,4,1,2,1,3,1,1,1,3,2,2,1,5,1,1,3,2,1,1,5,1,1,1,1,1,1,1,1,1,1,2,5,1,1,1,1,3,1,1,1,1,1,1,1,1,5,5,1
+			//};
+
+			string data =
+@"2199943210
+3987894921
+9856789892
+8767896789
+9899965678";
+
+			return ParseData(data);
+		}
+
+		private static List<string> ParseData(string data)
+		{
+			var rows = data.Split("\r\n");
+
+			var dataToReturn = new List<string>();
+
+			foreach (string row in rows)
+			{
+				dataToReturn.Add(row);
+			}
+
+			return dataToReturn;
+		}
+	}
+}
---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AdventOfCode2022
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var result = Day1.Run();
+			Console.WriteLine($"Result: {result}");
+		}
+	}
+}
